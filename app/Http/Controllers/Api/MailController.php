acd6ce98dd9dc785de529cc37740cc094517d448
Josequal <87031193+josequalgit@users.noreply.github.com>
2022-04-21 14:32:34 +0300

    test send email

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -12,13 +12,13 @@ class MailController extends Controller
         $data = array('name'=>"Virat Gandhi");
      
         Mail::send(['text'=>'mail'], $data, function($message) {
-           $message->to('abc@gmail.com', 'Tutorials Point')->subject
+           $message->to('qusai@josequal.com', 'Tutorials Point')->subject
               ('Laravel Basic Testing Mail');
            $message->from('xyz@gmail.com','Virat Gandhi');
         });
         echo "Basic Email Sent. Check your inbox.";
      }
-     
+
      public function html_email() {
         $data = array('name'=>"Virat Gandhi");
         Mail::send('mail', $data, function($message) {
