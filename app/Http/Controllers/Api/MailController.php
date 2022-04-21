f05623218c855832ce5c885bbbd736a414630801
Josequal <87031193+josequalgit@users.noreply.github.com>
2022-04-21 14:38:16 +0300

    test send email

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -12,32 +12,12 @@ class MailController extends Controller
         $data = array('name'=>"Virat Gandhi");
      
         Mail::send(['text'=>'mail'], $data, function($message) {
-           $message->to($request->email, 'Tutorials Point')->subject
+           $message->to($request->email, 'Test Email')->subject
               ('Laravel Basic Testing Mail');
            $message->from('qusai@josequal.com','Josequal');
         });
         echo "Basic Email Sent. Check your inbox.";
      }
 
-     public function html_email() {
-        $data = array('name'=>"Virat Gandhi");
-        Mail::send('mail', $data, function($message) {
-           $message->to('abc@gmail.com', 'Tutorials Point')->subject
-              ('Laravel HTML Testing Mail');
-           $message->from('xyz@gmail.com','Virat Gandhi');
-        });
-        echo "HTML Email Sent. Check your inbox.";
-     }
-
-     public function attachment_email() {
-        $data = array('name'=>"Virat Gandhi");
-        Mail::send('mail', $data, function($message) {
-           $message->to('abc@gmail.com', 'Tutorials Point')->subject
-              ('Laravel Testing Mail with Attachment');
-           $message->attach('C:\laravel-master\laravel\public\uploads\image.png');
-           $message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
-           $message->from('xyz@gmail.com','Virat Gandhi');
-        });
-        echo "Email Sent with attachment. Check your inbox.";
-     }
+    
 }
