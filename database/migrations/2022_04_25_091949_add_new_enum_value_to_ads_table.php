8ea7c220bb8ccb1efc9ef54db6be9893b0d43e3d
Josequal <87031193+josequalgit@users.noreply.github.com>
2022-04-25 12:28:46 +0300

    change the enum value

---
+++ database/migrations/2022_04_25_091949_add_new_enum_value_to_ads_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement("ALTER TABLE `ads` CHANGE `relation` `relation` ENUM('owner','employee','advertising company', 'other') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'other';");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->enum('relation',['owner','employee','advertising company','other'])->default('owner');
+        });
+    }
+};
