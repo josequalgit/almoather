8ce7e7c8297387a004dad6f7464521fa57b3e8fd
Josequal <87031193+josequalgit@users.noreply.github.com>
2022-05-11 18:10:29 +0300

    before payment

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -727,7 +727,7 @@ class AdController extends Controller
                         return [
 							'id'=>$item->influencers->id,
                             'name'=>$item->influencers->first_name.' '.$item->influencers->middle_name.' '.$item->influencers->last_name,
-                            'image'=>$inf->users->InfulncerImage?$inf->users->InfulncerImage:null,
+                            'image'=>$item->influencers->users->InfulncerImage?$item->influencers->users->InfulncerImage:null,
                             'match'=>$item->match,
                             'status'=>$item->status
                         ];
