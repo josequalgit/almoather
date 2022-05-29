ab06dc325c29a68b444ae8e0d68b6a891ee65876
Josequal <github@josequal.com>
2022-05-29 11:53:10 +0300

    - server.js

--- server.js
+++ server.js
@@ -26,15 +26,9 @@ server.listen(config.server.port, () => {
     console.log('listening on port 3000');
 });
 
-const usersOnline = {};
-
 
 io.on("connect", (socket) => {
     console.log('connected');
-
-    const clientsInRoom = io.in('support').allSockets()
-    io.emit("get.online.users", clientsInRoom);
-
 
     var onevent = socket.onevent;
     socket.onevent = function (packet) {
@@ -46,20 +40,11 @@ io.on("connect", (socket) => {
 
     socket.on('disconnect', function () {
         console.log('disconnect');
-        delete usersOnline[socket.id];
-        socket.leave('support');
-        io.emit("user.offline", socket.userId);
     });
 
     socket.on('*', function (event, data) {
-        console.log('event: ' + event.includes('support'))
-        if (event == 'user.online.save') {
-            console.log('a user ' + data.userId + ' connected');
-            socket.userId = data.userId;
-            socket.join('support');
-            io.emit("user.online", socket.user_id);
-
-        } else {
+        console.log('event: ' + event.includes('support'));
+        if(event.includes('support')) {
             var CURRENT_TIMESTAMP = { toSqlString: function () { return 'CURRENT_TIMESTAMP()'; } };
 
             let messageData = {
@@ -71,18 +56,12 @@ io.on("connect", (socket) => {
                 updated_at: CURRENT_TIMESTAMP,
             };
 
-
-
-
-
             connection.query('INSERT INTO messages SET ?', messageData, function (error, results, fields) {
                 if (error) throw error;
             });
-
-            io.emit(event, data);
         }
 
-
+        io.emit(event, data);
 
     });
 });
