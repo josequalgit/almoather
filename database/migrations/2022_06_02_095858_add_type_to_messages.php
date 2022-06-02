33e5c024d974d1c6029e72cbee364459adc292f3
Josequal <87031193+josequalgit@users.noreply.github.com>
2022-06-02 14:58:56 +0300

    add text to messages

--- database/migrations/2022_06_02_095858_add_type_to_messages.php
+++ database/migrations/2022_06_02_095858_add_type_to_messages.php
@@ -11,7 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up() 
     {
         Schema::table('messages', function (Blueprint $table) {
             $table->enum('contentType',['text','file'])->default('text');
