ccb06024455f0681e1dbbd51c1cc8e5adf8d8b17
Josequal <github@josequal.com>
2022-06-16 10:38:43 +0300

    - influencers

--- resources/views/dashboard/ads/include/influencer_table.blade.php
+++ resources/views/dashboard/ads/include/influencer_table.blade.php
@@ -3,11 +3,10 @@
     <thead>
         <tr>
             <th>Image</th>
-            <th>Full name</th>
+            <th>Nickname</th>
             <th>Match</th>
             <th>Chosen</th>
             <th>Status</th>
-            <th>Accepted</th>
             <th>Action</th>
         </tr>
     </thead>
@@ -17,29 +16,14 @@
             <tr>
                 <td>
                     <div class="thumb">
-                        <img class="img-fluid inf-image" src="{{ $item->users->infulncerImage ? $item->users->infulncerImage['url'] : null }}" alt="">
+                        <img class="img-fluid inf-image" src="{{ $item->influencers->users ? $item->influencers->users->infulncerImage['url'] : null }}" alt="">
                     </div>
                 </td>
-                <td>{{ $item->full_name }}</td>
+                <td>{{ $item->influencers->nick_name }}</td>
                 <td>{{ $item->match }}%</td>
                 <td>{{ $item->chosen ? 'Yes' : 'No' }}</td>
                 <td>{{ $item->status }}</td>
                 <td>
-                    @if ($item->influencers->checkIfAccepted($data->id) == 1)
-                        Yes
-                    @elseif($item->influencers->checkIfAccepted($data->id) == 2)
-                        No Contract avalibale
-                    @else
-                        No
-                    @endif
-                </td>
-                <td>
-                    @if ($data->status == 'approve' || $data->status == 'fullpayment')
-                        <button {{ $item->influencers->checkIfAccepted($data->id) == 1 ? 'disabled' : '' }} type="button" onclick="seeContract('{{ $data->contacts->content }}','{{ $item->influencers->id }}')" class="btn btn-secondary">
-                            <i class="bx bx-send "></i>
-                        </button>
-                    @endif
-
                     <button type="button" onclick="getUnchosenInfulncers('{{ $item->influencers->id }}')" class="btn btn-secondary">
                         <i class="bx bx-transfer"></i>
                     </button>
