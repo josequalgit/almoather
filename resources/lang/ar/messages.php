7211ec3a2ba996518fa0ed9face5cde4618cb765
Yousef Qaoud <yousef.qaood@gmail.com>
2022-06-29 01:38:03 +0300

    - translate

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -2,13 +2,16 @@
 
 return [
     'api'=>[
-        'not_found'=>'Not Found',
-        'influencer_not_found'=>'influencer is not authorized to get ads with this status',
-        'Male' => 'ذكر',
-        'Female' => 'أنثى',
-        'service' => 'خدمة',
-        'product' => 'منتج',
-        'online' => 'اونلاين',
-        'onsite' => 'في الموقع',
+        'not_found'                 => 'لم يتم العثور على المعلومات المطلوبة',
+        'influencer_not_found'      => 'المؤثر غير مصرح له بالحصول على إعلانات بهذا التمثال',
+        'Male'                      => 'ذكر',
+        'Female'                    => 'أنثى',
+        'service'                   => 'خدمة',
+        'product'                   => 'منتج',
+        'online'                    => 'اونلاين',
+        'onsite'                    => 'في الموقع',
+        'wrong_email_password'      => 'اسم المستخدم او كلمة المرور غير صحيحة',
+        'user_data'                 => 'تم تسجيل الدخول بنجاح. مرحبًا بعودتك',
+        'successfully_logged_out'   =>'تم تسجيل الخروج بنجاح',
     ]
 ];
