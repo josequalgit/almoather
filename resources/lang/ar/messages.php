157ef5386d07cf8e797a90b13d81fc5844a636e8
Yousef Qaoud <yousef.qaood@gmail.com>
2022-06-29 01:30:13 +0300

    - translate

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -6,8 +6,8 @@ return [
         'influencer_not_found'=>'influencer is not authorized to get ads with this status',
         'Male' => 'ذكر',
         'Female' => 'أنثى',
-        'Service' => 'خدمة',
-        'Product' => 'منتج',
+        'service' => 'خدمة',
+        'product' => 'منتج',
         'online' => 'اونلاين',
         'onsite' => 'في الموقع',
     ]
