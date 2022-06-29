db947bec616ec64cf67d094ef94c785926bc4b0f
Yousef Qaoud <yousef.qaood@gmail.com>
2022-06-29 04:30:04 +0300

    -pdf

--- resources/views/dashboard/contract/pdf.blade.php
+++ resources/views/dashboard/contract/pdf.blade.php
@@ -1,30 +1,141 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title ?? ''}}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('main2/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/pdf.css') }}">
+    <title>{{ $title ?? '' }}</title>
+
+    <style>
+
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+        
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin-right: -15px;
+            margin-left: -15px;
+        }
+
+        .text-left {
+            text-align: left !important;
+        }
+
+        hr {
+            margin-top: 1rem;
+            margin-bottom: 1rem;
+            border: 0;
+        }
+
+        .container {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+            max-width: 1140px;
+        }
+
+        .mb-0, .my-0 {
+    margin-bottom: 0 !important;
+    margin-top: 6px;
+    font-weight: 500;
+}
+
+        .col-6 {
+            flex: 0 0 50%;
+            max-width: 50%;
+        }
+
+        .col-12 {
+            flex: 0 0 100%;
+            max-width: 100%;
+        }
+
+        .text-center {
+            text-align: center !important;
+        }
+
+        .text-dark {
+            color: #394c62 !important;
+        }
+
+        .text-right {
+            text-align: right !important;
+        }
+
+
+        .col-6,
+        .col-12 {
+            position: relative;
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+
+        body {
+            background-color: #fff;
+            font-family: 'Rubik', 'Tajawal';
+            background-repeat: no-repeat;
+            background-position: top right;
+        }
+
+        .direction-rtl {
+            direction: rtl;
+        }
+
+        .logo {
+            width: 250px;
+        }
+
+        .devider {
+            border-top: 3px solid #8486AB;
+        }
+
+        a.website-link {
+            font-size: 20px;
+            font-weight: bold;
+            color: #8486AB;
+            text-decoration: underline;
+        }
+
+        .contract-content {
+            position: relative;
+        }
+
+        .contract-content:after {
+            content: "";
+            background-image: url(../../img/avatars/logo-almuaather.png);
+            opacity: 0.15;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            position: absolute;
+            z-index: -1;
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-size: 100%;
+        }
+    </style>
 </head>
-<body style="background-image: url('{{ asset('img/avatars/pdf-bg.png'); }}')">
+
+<body style="background-image: url('{{ asset('img/avatars/pdf-bg.png') }}')">
     <htmlpageheader name="page-header">
         <header class="container">
             <div class="row">
                 <div class="col-12">
-                    <img src="{{ asset('img/avatars/almuather-logo.png'); }}" alt="Almuather" class="logo">
+                    <img src="{{ asset('img/avatars/almuather-logo.png') }}" alt="Almuather" class="logo">
                 </div>
             </div>
             <hr class="devider">
         </header>
-        
+
     </htmlpageheader>
-    <div class="container text-right direction-rtl text-dark contract-content" >
+    <div class="container text-right direction-rtl text-dark contract-content">
         {!! $contract !!}
     </div>
     <htmlpagefooter name="page-footer">
@@ -49,9 +160,13 @@
                     <p class="mb-0">info@Almuaathir.com</p>
                     <p class="mb-0">ص . ب : 11799</p>
                 </div>
-                <div class="col-12"><p class="text-center"><a href="www.almuaathir.com" target="_blank" class="website-link"> www.almuaathir.com</a></p></div>
+                <div class="col-12">
+                    <p class="text-center"><a href="www.almuaathir.com" target="_blank" class="website-link">
+                            www.almuaathir.com</a></p>
+                </div>
             </div>
         </footer>
     </htmlpagefooter>
 </body>
+
 </html>
