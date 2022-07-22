4abd0ac58009ae13d054d9236439b7f20ea6690d
Yousef Qaoud <yousef.qaood@gmail.com>
2022-07-22 23:00:54 +0300

    - add scenario to response

--- app/Http/Traits/AdResponse.php
+++ app/Http/Traits/AdResponse.php
@@ -163,6 +163,7 @@ trait AdResponse
             $basicResponse['date'] = $contractData && $contractData->created_at ?  $contractData->created_at->diffForHumans() : '';
 
             $basicResponse['showExecution'] = $contractData && $contractData->date && !$contractData->date->gt(Carbon::now()) ? true : false;
+            $basicResponse['scenario'] = $contractData->scenario;
         }else{
             $basicResponse['hasReturn'] = $ad->relation_id == 3 ? false : true;
         }
